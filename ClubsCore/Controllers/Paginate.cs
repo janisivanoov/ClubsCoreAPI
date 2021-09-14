8936c6022548e95e1643f04dc2aebf5e3b919cca
Janis <janisivanov@icloud.com>
2021-09-14 13:19:14 +0300

    Added Paginate.cs, StudentDTO. Deleted Parameters(useless)

---
+++ ClubsCore/Controllers/Paginate.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using ClubsCore.Paging;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClubsCore.Controllers
+{
+    public class Paginate
+    {
+        protected object _query;
+        protected object _queryparameters;
+        protected IMapper _mapper;
+
+        public List<TDto> Paginate1<TDto>(IQueryable query, QueryParameters queryparameters)
+        {
+            return query.ProjectTo<TDto>(_mapper.ConfigurationProvider)
+                        .Skip((queryparameters.PageNumber - 1) * queryparameters.PageSize)
+                        .Take(queryparameters.PageSize)
+                        .ToList();
+        }
+    }
+}
---
+++ ClubsCore/Mapping/DTO/StudentDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ClubsCore.Mapping.DTO
+{
+    public class StudentDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
