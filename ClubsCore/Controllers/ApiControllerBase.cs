a9f3fc0bdab45fafc8fadfae7ee58621bbbd032f
Janis <janisivanov@icloud.com>
2021-09-20 12:19:23 +0300

    Added some comments[ApiControllerBase], deleted useless staff BaseEntity

--- ClubsCore/Controllers/ApiControllerBase.cs
+++ ClubsCore/Controllers/ApiControllerBase.cs
@@ -24,17 +24,17 @@ namespace ClubsCore.Controllers
         public List<TDto> Paginate<TDto>(IQueryable query, QueryClubParameters QueryClubParameters)
         {
             return query.ProjectTo<TDto>(_mapper.ConfigurationProvider)
-                        .Skip((QueryClubParameters.PageNumber - 1) * QueryClubParameters.PageSize)
-                        .Take(QueryClubParameters.PageSize)
-                        .ToList();
+                        .Skip((QueryClubParameters.PageNumber - 1) * QueryClubParameters.PageSize) //Skippin adding process PageNumber and PageSize
+                        .Take(QueryClubParameters.PageSize) //Using PageSize
+                        .ToList(); //Sending to List
         }
 
         public List<TDTO> Paginate<TDTO>(IQueryable<TDTO> query, QueryStudentParameters QueryStudentParameters)
         {
             return query.ProjectTo<TDTO>(_mapper.ConfigurationProvider)
-                                .Skip((QueryStudentParameters.PageNumber - 1) * QueryStudentParameters.PageSize)
-                                .Take(QueryStudentParameters.PageSize)
-                                .ToList();
+                                .Skip((QueryStudentParameters.PageNumber - 1) * QueryStudentParameters.PageSize) //Skipping adding process PageNumber and PageSize
+                                .Take(QueryStudentParameters.PageSize) //Using PageSize
+                                .ToList(); //Sending to List
         }
     }
 }
